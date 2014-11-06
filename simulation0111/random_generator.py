a8e485d7fe0ce5c0230685275b3f3fe80efa3204
wzheng@eecs.berkeley.edu <wzheng13@gmail.com>
2014-11-06 14:10:03 -0800

    Added option to draw network graph; corrected small bug in graph generation

--- simulation0111/random_generator.py
+++ simulation0111/random_generator.py
@@ -3,12 +3,39 @@ import numpy.random
 import yaml
 import sys
 import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-def Main(n, m, hosts, ctrlrs, stype, htype, ctype, ctrlAddr):
+def draw_graph(out, gfile):
+   G = nx.Graph()
+   for key in out:
+      if key == "links":
+         links = out[key]
+         for l in links:
+            p = l.split('-')
+            G.add_edge(p[1], p[0], type="link")
+      else:
+         G.add_node(key, node_type=out[key]['type'])
+   pos = nx.spring_layout(G)
+   nl = [x[0] for x in G.nodes(data=True) if x[1]['node_type'].find("Switch") >= 0]
+   nx.draw_networkx_nodes(G,pos,nodelist=nl,node_color="#A0CBE2")
+   nl = [x[0] for x in G.nodes(data=True) if x[1]['node_type'].find("Control") >= 0]
+   nx.draw_networkx_nodes(G,pos,nodelist=nl,node_color="red")
+   nl = [x[0] for x in G.nodes(data=True) if x[1]['node_type'].find("Host") >= 0]
+   nx.draw_networkx_nodes(G,pos,nodelist=nl,node_color="green")
+   nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
+   labels = {}
+   for x in G.nodes(data=True):
+      labels[x[0]] = x[0]
+   nx.draw_networkx_labels(G, pos, labels)
+   plt.savefig(gfile)
+
+def Main(n, m, hosts, ctrlrs, stype, htype, ctype, ctrlAddr, gfile):
    """Generate a graph with n switches (and m edges per node on average), host hosts
    and controller controllers.
    Hosts and controllers are singly connected"""
-   g = nx.erdos_renyi_graph(n, m)
+   g = nx.erdos_renyi_graph(n, m*1.0/n)
    out = {}
    for node in g.nodes_iter():
      out['s%d'%(node + 1)] = {'type': stype} 
@@ -28,6 +55,10 @@ def Main(n, m, hosts, ctrlrs, stype, htype, ctype, ctrlAddr):
        out[ctrl_id] = {'type': ctype}
      s = numpy.random.randint(n)
      out['links'].append("s%d-%s"%(s+1, ctrl_id))
+
+   if gfile != "":
+      draw_graph(out, gfile)
+         
    return yaml.dump(out)
 
 if __name__ == "__main__": 
@@ -45,6 +76,7 @@ if __name__ == "__main__":
   parser.add_argument("-ht", help="Host type: Host, HBHost")
   parser.add_argument("-ct", help="Controller type: SpControl, CoordinatingControl, HBControl")
   parser.add_argument("--ctrl-need-addr", dest="if_ctrl_addr", action='store_true', default=False, help="Indicates whether the controllers need addresses")
+  parser.add_argument("-g", dest="gfile", default="", help="If enabled, writes visualization of the network to gfile")
   args = parser.parse_args()
 
   print Main(args.n, \
@@ -54,4 +86,6 @@ if __name__ == "__main__":
              args.st, \
              args.ht, \
              args.ct, \
-             args.if_ctrl_addr)
+             args.if_ctrl_addr, \
+             args.gfile
+          )
