1c0bb6af84bae320eb33c1c91fafdc6fc4d07830
Aurojit <apanda@cs.berkeley.edu>
2014-11-22 18:35:29 -0800

    Add a script to run groups of things more easily

---
+++ simulation0111/run.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+prefix=$1
+trace=$2
+outprefix=$3
+for idx in 0 3 4
+  do
+    file="$prefix$idx"
+    file+=".yaml"
+    echo $file
+    python -u perturbation.py $file $trace 750.0 150.0 1500.0 150.0 > $outprefix/$idx &
+  done
