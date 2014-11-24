a41fcba349abfc6d8df6caf4b7317479ee06fe06
Aurojit <apanda@cs.berkeley.edu>
2014-11-23 20:44:10 -0800

    Latency

--- simulation0111/result_proc/perturb_latency_stats.py
+++ simulation0111/result_proc/perturb_latency_stats.py
@@ -32,7 +32,7 @@ for l in f:
     mean = float(p[1])
     latency = []
   elif re.match("^\d+?\.\d+?$", p[0]):
-    clatency = eval(l.strip().split(' ', 5)[-1])
+    clatency = eval(l.strip().split(' ', 4)[-1])
     if len(clatency) > 0:
        latency.extend(clatency)
     
