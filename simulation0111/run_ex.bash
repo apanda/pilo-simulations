f50709c6ad5f9a464f09a4d9df2280a050ebb4f7
Aurojit <apanda@cs.berkeley.edu>
2014-11-25 00:46:49 -0800

    Change ex

--- simulation0111/run_ex.bash
+++ simulation0111/run_ex.bash
@@ -8,9 +8,9 @@ for idx in 0 3 4 5
     echo $file
 #Usage: perturbation_extreme.py setup stable_time links_to_fail mean_recovery wait_at_end begin_mean_perturb
 #end_mean_perturn step_mean_perturb seed
-    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 150.0 300.0 150.0 42 > $outprefix/$idx.0 &
-    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 300.0 600.0 150.0 42 > $outprefix/$idx.1 &
-    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 600.0 900.0 150.0 42 > $outprefix/$idx.2 &
-    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 900.0 1200.0 150.0 42 > $outprefix/$idx.3 &
-    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 1200.0 1500.0 150.0 42 > $outprefix/$idx.4 &
+    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 150.0 300.0 150.0 42 > $outprefix/ex$idx.0 &
+    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 300.0 600.0 150.0 42 > $outprefix/ex$idx.1 &
+    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 600.0 900.0 150.0 42 > $outprefix/ex$idx.2 &
+    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 900.0 1200.0 150.0 42 > $outprefix/ex$idx.3 &
+    python -u perturbation_extreme.py $file 200.0 250 210.0 10000.0  750.0 1200.0 1500.0 150.0 42 > $outprefix/ex$idx.4 &
   done
