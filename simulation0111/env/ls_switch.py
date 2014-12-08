41d6cb2220f848ed10240b27cb4ab2e2102433a9
Aurojit <apanda@cs.berkeley.edu>
2014-12-07 18:17:35 -0800

    Stop nacking requests for link state
    This is safe because subsequent LS updates will trigger the same action

--- simulation0111/env/ls_switch.py
+++ simulation0111/env/ls_switch.py
@@ -69,7 +69,7 @@ class LSLeaderSwitch (LinkStateSwitch):
   
   def updateRules (self, source, match_action_pairs):
     if source != self.currentLeader:
-      packet = ControlPacket(self.cpkt_id, self.name, source, ControlPacket.NackUpdateRules, []) 
+      #packet = ControlPacket(self.cpkt_id, self.name, source, ControlPacket.NackUpdateRules, []) 
       self.Flood(None, packet)
       self.cpkt_id += 1
     else:
---
+++ simulation0111/run_ex_ls.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+prefix=$1
+outprefix=$2
+recovery=$3
+SEED="42"
+SAMPLINGRATE="25000"
+ENDTIME="600000.0"
+for idx in 0
+  do
+    file="$prefix$idx"
+    file+=".yaml"
+    echo $file
+#Usage: perturbation_extreme.py setup stable_time links_to_fail mean_recovery wait_at_end begin_mean_perturb
+#end_mean_perturn step_mean_perturb sampling rate seed
+    python -u perturbation_extreme.py $file 750.0 250 $recovery $ENDTIME 150.0 750.0 150.0 $SAMPLINGRATE $SEED > $outprefix/ex$idx.0 &
+    python -u perturbation_extreme.py $file 750.0 250 $recovery $ENDTIME 750.0 1500.0 150.0 $SAMPLINGRATE $SEED > $outprefix/ex$idx.1 &
+    #python -u perturbation_extreme.py $file 750.0 250 $recovery $ENDTIME 900.0 1500.0 150.0 $SAMPLINGRATE $SEED > $outprefix/ex$idx.2 &
+    #python -u perturbation_extreme.py $file 750.0 250 $recovery $ENDTIME 900.0 1200.0 150.0 $SAMPLINGRATE $SEED > $outprefix/ex$idx.3 &
+    #python -u perturbation_extreme.py $file 750.0 250 $recovery $ENDTIME 1200.0 1500.0 150.0 $SAMPLINGRATE $SEED > $outprefix/ex$idx.4 &
+  done
