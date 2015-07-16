b110b40491c49f5b305f0057ded1593ad5b8bdff
Wenting Zheng <wzheng@eecs.berkeley.edu>
2015-07-16 11:10:10 -0700

    Changed params for fast bw measurement

--- simulation0111/bw_fast.sh
+++ simulation0111/bw_fast.sh
@@ -1,22 +1,22 @@
-exp=( bt/bt as-topos/1221 as-topos/1239 fat_tree/ft_4_arity/fat_tree_4_arity )
-config_files=( config_bt_fast.yml config_as_fast.yml config_as_fast.yml config_ft_fast.yml )
-out_name=( bt as_1221 as_1239 fat_tree_4_arity )
+exp=( bt/bt )
+config_files=( config_bt_fast.yml  )
+out_name=( bt  )
 
 for i in ${!exp[@]}; do
     e=${exp[$i]}
     cf=${config_files[$i]}
     fname=${out_name[$i]}
     num=3
-    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 30000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.30s"
+    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 30000 10240000 10240000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.30s"
 
-    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 30000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.30s
+    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 30000 10240000 10240000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.30s
 
-    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.1min"
+    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.1min"
 
-    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.1min
+    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.1min
 
-    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw/$fname$num.output.10min"
+    echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.10min"
 
-    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw/$fname$num.output.10min
+    python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw_fast/$fname$num.output.10min
 
 done
