03000fcbdb9f77b4db1c59e1fe7cbf21e80a334d
Wenting Zheng <wzheng@eecs.berkeley.edu>
2015-07-16 11:11:41 -0700

    Multi-link finished

--- simulation0111/bw.sh
+++ simulation0111/bw.sh
@@ -1,6 +1,11 @@
-exp=( bt/bt as-topos/1221 as-topos/1239 fat_tree/ft_4_arity/fat_tree_4_arity )
-config_files=( config_bt.yml config_as.yml config_as.yml config_ft.yml )
-out_name=( bt as_1221 as_1239 fat_tree_4_arity )
+#exp=( bt/bt as-topos/1221 as-topos/1239 fat_tree/ft_4_arity/fat_tree_4_arity )
+#config_files=( config_bt.yml config_as.yml config_as.yml config_ft.yml )
+#out_name=( bt as_1221 as_1239 fat_tree_4_arity )
+
+exp=( fat_tree/ft_4_arity/fat_tree_4_arity )
+config_files=( config_ft.yml )
+out_name=( fat_tree_4_arity )
+
 
 for i in ${!exp[@]}; do
     e=${exp[$i]}
