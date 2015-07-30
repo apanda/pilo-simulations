c4acfca2621a02453c48f6b3634f19bc75c97a89
Wenting Zheng <wzheng@eecs.berkeley.edu>
2015-07-30 11:16:03 -0700

    Latest results for fat tree with more reasonable parameters

--- simulation0111/bw_fast.sh
+++ simulation0111/bw_fast.sh
@@ -1,10 +1,10 @@
-exp=( bt/bt as-topos/1221 fat_tree/ft_4_arity/fat_tree_4_arity )
-config_files=( config_bt_fast.yml config_as_fast.yml config_ft_fast.yml )
-out_name=( bt as_1221 fat_tree_4_arity )
+# exp=( bt/bt as-topos/1221 fat_tree/ft_4_arity/fat_tree_4_arity )
+# config_files=( config_bt_fast.yml config_as_fast.yml config_ft_fast.yml )
+# out_name=( bt as_1221 fat_tree_4_arity )
 
-#exp=( fat_tree/ft_4_arity/fat_tree_4_arity )
-#config_files=( config_ft.yml )
-#out_name=( fat_tree_4_arity )
+exp=( fat_tree/ft_8_arity/fat_tree_8_arity_4_controller fat_tree/ft_8_arity/fat_tree_8_arity)
+config_files=( config_ft.yml config_ft.yml )
+out_name=( fat_tree_8_arity_4_controller fat_tree_8_arity_8_controller )
 
 
 for i in ${!exp[@]}; do
@@ -12,18 +12,10 @@ for i in ${!exp[@]}; do
     cf=${config_files[$i]}
     fname=${out_name[$i]}
     num=3
-    dir="outputs/bw_fast"
-    echo "python perturbation_noboot.py $e$num.yaml 0 2000 600000 5000 10240000 10240000 1000 42 true $cf > $dir/$fname$num.output.5s"
+    dir="outputs/bw_normal"
+    echo "python perturbation_noboot.py $e$num.yaml 0 20000 600000 60000 10240000 10240000 1000 42 true $cf > $dir/$fname$num.output"
 
-    stdbuf -o0 python perturbation_noboot.py $e$num.yaml 0 2000 600000 5000 10240000 10240000 1000 42 true $cf > $dir/$fname$num.output.5s
-
-    # echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.1min"
-
-    # python perturbation_noboot.py $e$num.yaml 0 300000 1800000 60000 10240000 10240000 1000 42 true $cf > outputs/bw/$fname$num.output.1min
-
-    # echo "python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw/$fname$num.output.10min"
-
-    # python perturbation_noboot.py $e$num.yaml 0 300000 1800000 600000 10240000 102400000 1000 42 true $cf > outputs/bw/$fname$num.output.10min
+    stdbuf -o0 python perturbation_noboot.py $e$num.yaml 0 20000 600000 60000 10240000 10240000 1000 42 true $cf > $dir/$fname$num.output
 
 done
 
